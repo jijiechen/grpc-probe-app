9af7437fd7609549537b28ec3cd85acc787a5e61
Jay Chen <1180092+jijiechen@users.noreply.github.com>
2024-04-15 16:24:37 +0800

    Added a helloworld greeter gRPC server

---
+++ main.go
@@ -0,0 +1,42 @@
+// Copied from https://github.com/grpc/grpc-go/blob/master/examples/helloworld/greeter_server/main.go
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	pb "github.com/jijiechen/grpc-probe-app/proto"
+	"google.golang.org/grpc"
+)
+
+var (
+	port = flag.Int("port", 5085, "The server port")
+)
+
+// server is used to implement helloworld.GreeterServer.
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received: %v", in.GetName())
+	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
+}
+
+func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
